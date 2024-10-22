2ce6ad3f2b8a1d4efca75818c2178ae1f29a6b3a
Lakshya Tyagi <lakshyatyagi2005@gmail.com>
2024-10-22 14:51:35 +0530

    Complete

--- app.js
+++ app.js
@@ -10,11 +10,18 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", controllers.getCategoriesNames);
-
 app.get("/aircrafts", controllers.getAircraftItems);
 app.get("/movies", controllers.getMovieItems);
 app.get("/devices", controllers.getDeviceItems);
 app.get("/aircrafts/add", controllers.getAddAircraft);
+app.get("/devices/add", controllers.getAddDevice);
+app.get("/movies/add", controllers.getAddMovie);
+app.post("/aircrafts/add", controllers.postAddAircraft);
+app.post("/devices/add", controllers.postAddDevice);
+app.post("/movies/add", controllers.postAddMovie);
+app.get("/aircrafts/delete/:id", controllers.deleteAircraftItem);
+app.get("/devices/delete/:id", controllers.deleteDeviceItem);
+app.get("/movies/delete/:id", controllers.deleteMovieItem);
 
 app.listen(3000, () => {
   console.log("Port 3000");
--- db/queries.js
+++ db/queries.js
@@ -30,6 +30,47 @@ async function fetchDeviceImages() {
   let { rows } = await pool.query("SELECT img FROM devices;");
   return rows;
 }
+async function addAircraft(name, img) {
+  await pool.query("INSERT INTO aircrafts (name,img) VALUES ($1,$2);", [
+    name,
+    img,
+  ]);
+}
+async function addDevice(name, img) {
+  await pool.query("INSERT INTO devices (name,img) VALUES ($1,$2);", [
+    name,
+    img,
+  ]);
+}
+async function addMovie(name, img) {
+  await pool.query("INSERT INTO movies (name,img) VALUES ($1,$2);", [
+    name,
+    img,
+  ]);
+}
+async function deleteAircraft(id) {
+  await pool.query("DELETE FROM aircrafts WHERE id=$1", [id]);
+}
+async function deleteMovie(id) {
+  await pool.query("DELETE FROM movies WHERE id=$1", [id]);
+}
+async function deleteDevice(id) {
+  await pool.query("DELETE FROM devices WHERE id=$1", [id]);
+}
+async function fetchAircraftId() {
+  const { rows } = await pool.query("SELECT id FROM aircrafts;");
+  return rows;
+}
+
+async function fetchDeviceId() {
+  const { rows } = await pool.query("SELECT id FROM devices;");
+  return rows;
+}
+
+async function fetchMovieId() {
+  const { rows } = await pool.query("SELECT id FROM movies;");
+  return rows;
+}
 
 module.exports = {
   fetchTableNames,
@@ -39,4 +80,13 @@ module.exports = {
   fetchMovieImages,
   fetchDeviceImages,
   fetchDevices,
+  addAircraft,
+  addDevice,
+  addMovie,
+  deleteAircraft,
+  deleteDevice,
+  deleteMovie,
+  fetchDeviceId,
+  fetchMovieId,
+  fetchAircraftId,
 };
